00b881d5a279aa654f64e09733f83240e0f925cc
yangshifu <yangshifu@sensetime.com>
2018-07-09 15:09:06 +0800

    modify config

--- s_config.py
+++ s_config.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 
 # 路径
-PATH_BASE = '/opt/test_tools/faceunlock_test_general'   # 梁鼎的批处理工具路径
-PATH_DATA = '/home/andrew/code/data/data_set'       # 数据集根路径
+PATH_BASE = '/mnt/lustre/yangshifu/faceunlock_test_general'   # 梁鼎的批处理工具路径
+PATH_DATA = '/mnt/lustre/yangshifu/testset'       # 数据集根路径
 PATH_DATA_2D = os.path.join(PATH_DATA, '2d')    # 深圳2d数据集路径
 PATH_DATA_3D = os.path.join(PATH_DATA, '3d')    # 深圳3d数据集路径
 
