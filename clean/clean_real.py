1d8454e732b58269595ae8bc4ad100af54671669
Yang Shifu <yangshifu@DOMAIN.SENSETIME.COM>
2018-08-13 19:07:39 +0800

    delete real person to 200

--- clean/clean_real.py
+++ clean/clean_real.py
@@ -8,6 +8,7 @@ Created on
 import argparse
 import os
 from pathlib import Path
+import random
 
 
 desc = """
@@ -22,10 +23,10 @@ args = parser.parse_args()
 data = Path(args.dataset)
 dict_real = {}
 for f in data.rglob('*Real'):
-    # 假人
+    # 真人
     files_real = list(f.glob('*.yuv'))
     count_real = len(files_real)
-    # 真人
+    # 假人
     files_hack = list(Path(str(f).replace('Real', 'Hack')).glob('*.yuv'))
     count_hack = len(files_hack)
     person = f.parent.name
@@ -34,3 +35,26 @@ for f in data.rglob('*Real'):
 
 print("--------------------------------------------")
 count_person = dict_real.values()
+
+person_20 = list(filter(lambda x: x < 20, count_person))
+person_200 = list(filter(lambda x: x > 200, count_person))
+
+for item in dict_real:
+    if dict_real[item] in person_20:
+        print("{}: {}".format(item, dict_real[item]))
+        # 删除真人数小于20张的人
+        # os.removedirs()
+    if dict_real[item] in person_200:
+        print("{}: {}".format(item, dict_real[item]))
+        # 真人数超过200时，随机删除到200张真人
+        dir_person = next(data.rglob('{}'.format(item)))
+        dir_person_real = dir_person / 'Real'
+        person_real = list(map(str, dir_person_real.rglob('*.yuv')))       # 真人
+        person_real_delete = random.choices(person_real, k=(dict_real[item]-200))      # 随机选取（总数-200）张图片
+        for f in person_real_delete:
+            os.remove(f)
+            # 删除同名文件的jpg
+            try:
+                os.remove(f.strip('.yuv') + '.jpg')
+            except FileNotFoundError:
+                continue
